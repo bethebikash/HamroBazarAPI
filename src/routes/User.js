1a678f8cb075dc373de8813f6067b7b2590afe25
bethebikash <bhattaraibikash55@gmail.com>
2020-01-05 18:28:52 +0545

    Hashing User password

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const router = new express.Router()
+const bcrypt = require('bcryptjs');
 const User = require('../models/User')
 
 router.post('/users/signup', (req, res, next) => {
-    const user = req.body
-        User.create(user).then(() => {
+    let password = req.body.password;
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) {
+            let err =  new Error('Could not hash!');
+		err.status = 500;
+		return next(err);
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            phone: req.body.phone,
+            mobile: req.body.mobile,
+            address1: req.body.address1,
+            address2: req.body.address2,
+            address3: req.body.address3,
+            image: req.body.image
+        }).then(() => {
             res.status(200).json({status: 'User created successfully.'})
         }).catch(next);
+    });
 });
 
 module.exports = router
